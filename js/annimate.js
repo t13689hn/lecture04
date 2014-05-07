6fa3e2cb688f13af575d141c41a1bfd68b005192
t13689hn <t13689hn@mcdamac02.sfc.keio.ac.jp>
2014-05-07 16:25:34 +0900

    2014/05/07
    komento no tuika

--- js/annimate.js
+++ js/annimate.js
@@ -1,25 +1,30 @@
-var endFlipFoxkeh = function(){
-	var foxkeh = document.querySelector("#slidein-foxkeh img");
-	foxkeh.removeEventListener("animationend", endFlipFoxkeh);
+var endFlipFoxkeh //endFlipFoxkehという情報の定義づけ
+= function(){
+	var foxkeh //foxkehとはどれかを定義させる
+		= document.querySelector("#slidein-foxkeh img");//document内にある"#slidein-foxkeh img"という情報を使用
+	foxkeh.removeEventListener("animationend", endFlipFoxkeh);//animationendで動作を終了させる
 
-	foxkeh.setAttribute("class", "");
+	foxkeh.setAttribute("class", "");//上段で説明されたfoxkehをwoclassで表す
 };
 
-var endSlideinFoxkehAndStartFlipFoxkeh = function(){
-	var foxkeh = document.querySelector("#slidein-foxkeh img");
+var endSlideinFoxkehAndStartFlipFoxkeh //endSlideinFoxkehAndStartFlipFoxkehという動作の定義付け
+= function(){
+	var foxkeh = document.querySelector("#slidein-foxkeh img");//document内にある"#slidein-foxkeh img"という情報を使用
 
-	foxkeh.removeEventListener("animationend", endSlideinFoxkehAndStartFlipFoxkeh);
-	foxkeh.addEventListener("animationend", endFlipFoxkeh);
+	foxkeh.removeEventListener("animationend", endSlideinFoxkehAndStartFlipFoxkeh);//"animationend", endSlideinFoxkehAndStartFlipFoxkehの動作を妨げる
+	foxkeh.addEventListener("animationend", endFlipFoxkeh);//"animationend", endFlipFoxkehの動作を開始させる。上段とこの段の２段によって、動作を繰り返し行う鵜ことを可能にしている
 
-	foxkeh.setAttribute("class", "flip");
+	foxkeh.setAttribute("class", "flip");//"class"を"flip",つまり回転させる
 };
 
-var startSlideinFoxkeh = function(){
-	var foxkeh = document.querySelector("#slidein-foxkeh img");
-	foxkeh.setAttribute("class", "slidein");
+var startSlideinFoxkeh//startSlideinFoxkehという動作の定義付け
+ = function(){
+	var foxkeh = document.querySelector("#slidein-foxkeh img");//document内にある"#slidein-foxkeh img"という情報を使用
+	foxkeh.setAttribute("class", "slidein");siyou//"class"を"slidein",つまり右からすべりこむように登場させる
 
-	foxkeh.addEventListener("animationend", endSlideinFoxkehAndStartFlipFoxkeh);
+	foxkeh.addEventListener("animationend", endSlideinFoxkehAndStartFlipFoxkeh);//"animationend", endFlipFoxkehの動作を開始させる
 };
 
-var slideinFoxkehButton = document.querySelector("#slidein-foxkeh button");
-slideinFoxkehButton.addEventListener("click", startSlideinFoxkeh);
+var slideinFoxkehButton//slideinFoxkehButtonという情報の定義付け
+ = document.querySelector("#slidein-foxkeh button");//document内にある"#slidein-foxkeh button"という情報を使用
+slideinFoxkehButton.addEventListener("click", startSlideinFoxkeh);//上段で定義したstartSlideinFoxkehBUttonのボタンをクリックされたとき、startSlideinFoxkehの動作を開始する。
---
+++ js/slidein.js
@@ -0,0 +1,10 @@
+var endSlideinFoxkeh = function(){
+	var foxkeh = document.querySelector("#slidein-foxkeh img");
+<<<<<<< HEAD
+	foxkeh.setAttribute("class", "slidein");
+	
+>>>>>>> origin/make_slidein_retriable
+};
+
+var slideinFoxkehButton = document.querySelector("#slidein-foxkeh button");
+slideinFoxkehButton.addEventListener("click", startSlideinFoxkeh);
